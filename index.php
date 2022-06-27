a839c7d6f9de682cd1a5ad549e85d932be3ffceb
Anderson0x07 <andersonadrianorro@ufps.edu.co>
2022-06-27 12:39:00 -0500

    MiProyecto

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Sitio Web Anderson0x07</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
+    
+    <script src='js/main.js'></script>
+</head>
+<body id="index">
+    
+    <div id="lista">
+        <img src="/img/logo-ufps.png" width="500px"> 
+        <pre style="margin-top: 20px;margin-bottom: 0;" id="lista">Nombre: Anderson Adrian Orozco Rolon       Email: andersonadrianorro@ufps.edu.co       Teléfono: 3219238493
+    </div>
+
+    <h1 id="titulos">PÁGINA DE INICIO</h1>
+    
+    <p style="font-size: larger; text-align: center;" >
+        <i>A continuación puede navegar por las diferentes paginas para conocer más sobre mi:</i>
+    </p>
+    
+    <div id="navega">
+        <a href="/html/peliculas.html">
+            Peliculas Favoritas
+        </a> 
+        <a href="/html/deporte.html">
+            Deportes de Interes
+        </a>
+        <a href="/html/musica.html">
+            Música de Interes
+        </a>
+    </div>
+    <br><br>
+</body>
+
+<footer  id="footer-index">
+    <br><br><br>
+    <p>
+        Desarrollado por Anderson Adrian Orozco Rolon ® <br>
+        Ingeniero de Sistemas en Formación <br><br>
+    </p>
+    <h3>REDES SOCIALES</h3>
+    <a href="https://www.facebook.com/Anderson0x07" target="_blanck"><img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="facebook" width="50px"></a>
+    <a href="https://www.instagram.com/anderson_orozzco/?hl=es" target="_blanck"><img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c521.png" alt="instagram" width="50px"></a>
+    <a href="https://wa.link/9tvvr8" target="_blanck"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/WhatsApp_logo-color-vertical.svg/2048px-WhatsApp_logo-color-vertical.svg.png" alt="whatsapp" width="50px"></a>
+    <br><br><br>
+</footer>
+
+</html>
